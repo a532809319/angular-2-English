979b8055d5a5fd936d182703e60b5858c9f4517b
hukai <532809319@qq.com>
2017-02-24 09:27:34 +0800

    angular of  english  ngclass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { OtherComponent } from './other/other.component';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OtherComponent,
+    DatabindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  a:string='数据啊a';
+  b:string='数据啊b';
+
+}
